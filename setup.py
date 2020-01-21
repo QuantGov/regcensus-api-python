deeade4df90c6cdbf6cba3ef60691d88abbdfe9f
Jonathan Nelson <jrnecon16@gmail.com>
2020-01-21 09:45:18 -0500

    version 0.1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setup(
    name='regcensus',
-   version='0.1.0',
+   version='0.1.1',
    description='Python package for accessing data from the QuantGov API',
    url='https://github.com/QuantGov/regcensus-api-python',
    author='QuantGov',
